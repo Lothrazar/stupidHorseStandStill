fc3807ef2dc426f57d1decbb708e98a9c855bc2e
Sam <lothrazar@hotmail.com>
2026-04-15 08:48:26 -0700

    porting 1.21

--- src/main/java/com/lothrazar/horsestandstill/ModHorseStandStill.java
+++ src/main/java/com/lothrazar/horsestandstill/ModHorseStandStill.java
@@ -1,14 +1,15 @@
 package com.lothrazar.horsestandstill;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(ModHorseStandStill.MODID)
 public class ModHorseStandStill {
 
   public static final String MODID = "horsestandstill";
 
-  public ModHorseStandStill() {
-    MinecraftForge.EVENT_BUS.register(new EventHorseStandStill());
+  public ModHorseStandStill(IEventBus modEventBus) {
+    NeoForge.EVENT_BUS.register(new EventHorseStandStill());
   }
 }
