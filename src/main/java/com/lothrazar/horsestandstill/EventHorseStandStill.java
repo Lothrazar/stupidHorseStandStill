dfd298f33336da290fa6640d011112bb532a8329
Sam <lothrazar@hotmail.com>
2026-08-06 14:02:00 -0700

    porting 26.1 WIP

--- src/main/java/com/lothrazar/horsestandstill/EventHorseStandStill.java
+++ src/main/java/com/lothrazar/horsestandstill/EventHorseStandStill.java
@@ -1,7 +1,7 @@
 package com.lothrazar.horsestandstill;
 
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.entity.animal.equine.AbstractHorse;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
@@ -28,7 +28,7 @@ public class EventHorseStandStill {
       boolean isPlayerRiding = (horse.getControllingPassenger() instanceof Player);
       Level level = horse.level();
       if (emptyState) {
-        if (level.isClientSide) {
+        if (level.isClientSide()) {
           return;//no client side data or tracking needed 
         }
         if (isPlayerRiding) {
@@ -40,7 +40,7 @@ public class EventHorseStandStill {
         }
       }
       else if (ridingState) { ////////////// riding
-        if (level.isClientSide) {
+        if (level.isClientSide()) {
           return;
         }
         //player is riding
@@ -52,13 +52,13 @@ public class EventHorseStandStill {
           if (horse.isSaddled()) {
             horse.spawnAnim();
             horse.hurt(level.damageSources().magic(), 0F);
-            if (level.isClientSide) {
+            if (level.isClientSide()) {
               return;
             }
             setWaitingStateAndPos(horse);
           }
           else {
-            if (living.level().isClientSide) {
+            if (living.level().isClientSide()) {
               return; //no client side data or tracking needed 
             }
             clearState(horse);
@@ -68,7 +68,7 @@ public class EventHorseStandStill {
         //still riding i guess 
       }
       else if (isWaitingState) { ////////////// waiting
-        if (level.isClientSide) {
+        if (level.isClientSide()) {
           return;
         }
         if (horse.isSaddled() && horse.isAlive() && !horse.isInWater() && !horse.isSwimming()) {
